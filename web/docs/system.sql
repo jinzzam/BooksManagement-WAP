56a5c1c10908947ba0887940357e50fa60a3a53d
DESKTOP-Q4IFKLJ\jin <firstmind_@naver.com>
2018-01-20 03:23:18 +0900

    made table book

--- web/docs/system.sql
+++ web/docs/system.sql
@@ -11,3 +11,15 @@ select * from member;
 
 insert into member values('박유진', '19971119', 'f', 'youjin', '1234', 'firstmind_@naver.com', '01057192212');
 insert into member values('이정민', '19941207', 'm', 'jeongmin', '9984', 'min@naver.com', '01012345678');
+
+create table book(
+no varchar2(10) primary key,
+classfi varchar2(20) not null,
+name varchar2(100) not null,
+author varchar2(50) not null,
+translator varchar2(40),
+publisher varchar2(30) not null,
+checkoutdate varchar2(20),
+checkedout varchar2(1));
+
+select * from book;

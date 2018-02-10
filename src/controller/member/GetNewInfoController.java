5afa41e579eb6bb5f2de36d6116531af56923c19
DESKTOP-Q4IFKLJ\jin <firstmind_@naver.com>
2018-02-10 21:16:48 +0900

    strated add modify info service by jin

---
+++ src/controller/member/GetNewInfoController.java
@@ -0,0 +1,50 @@
+package controller.member;
+
+import model.Service.MemberService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/modify")
+public class ModifyController extends HttpServlet {
+
+    protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        HttpSession session = request.getSession();
+        MemberService memberService = MemberService.getInstance();
+
+        if (session.getAttribute("id") != null) {
+            String id = (String) session.getAttribute("id");
+            String name = memberService.getMember(id).getName();
+            String password = memberService.getMember(id).getPassword();
+            String email = memberService.getMember(id).getEmail();
+            String phone = memberService.getMember(id).getPhone();
+
+            request.setAttribute("id", id);
+            request.setAttribute("name", name);
+            request.setAttribute("password", password);
+            request.setAttribute("email", email);
+            request.setAttribute("phone", phone);
+
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("ModifyInfo.jsp");
+            requestDispatcher.forward(request, response);
+            return;
+        }
+        response.sendRedirect("login");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doProcess(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doProcess(request, response);
+    }
+}
--- src/controller/member/ModifyInfoController.java
+++ src/controller/member/ModifyInfoController.java
@@ -10,34 +10,38 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet("/modify")
-public class ModifyController extends HttpServlet {
+@WebServlet("/modify-info")
+public class ModifyInfoController extends HttpServlet {
 
     protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setCharacterEncoding("UTF-8");
-        PrintWriter out = response.getWriter();
         HttpSession session = request.getSession();
         MemberService memberService = MemberService.getInstance();
-        if (session.getAttribute("id") != null) {
-            String id = (String) session.getAttribute("id");
-            String name = memberService.getMember(id).getName();
-            String password = memberService.getMember(id).getPassword();
-            String email = memberService.getMember(id).getEmail();
-            String phone = memberService.getMember(id).getPhone();
-
-            request.setAttribute("id", id);
-            request.setAttribute("name", name);
-            request.setAttribute("password", password);
-            request.setAttribute("email", email);
-            request.setAttribute("phone", phone);
-
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("ModifyInfo.jsp");
-            requestDispatcher.forward(request, response);
-            return;
+        String id = (String) session.getAttribute("id");
+        boolean ch = false;
+        if (request.getAttribute("newPassword") != null) {
+            String newPassword = (String) request.getAttribute("newPassword");
+            memberService.getMember(id).setPassword(newPassword);
+            ch = true;
         }
-        response.sendRedirect("login");
+
+        if (request.getAttribute("newEmail") != null) {
+            String newEmail = (String) request.getAttribute("newEmail");
+            memberService.getMember(id).setEmail(newEmail);
+            ch = true;
+        }
+
+        if (request.getAttribute("newPhone") != null) {
+            String newPhone = (String) request.getAttribute("newPhone");
+            memberService.getMember(id).setPhone(newPhone);
+            ch = true;
+        }
+
+        request.setAttribute("ch", ch);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("ChangeResult.jsp");
+        requestDispatcher.forward(request, response);
+
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {

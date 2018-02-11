0578e241709c443c12294198fd89206a1f145c60
DESKTOP-Q4IFKLJ\jin <firstmind_@naver.com>
2018-02-11 22:13:38 +0900

    회원가입 인증 이메일 보내기 성공

--- src/controller/member/JoinController.java
+++ src/controller/member/JoinController.java
@@ -3,6 +3,10 @@ package controller.member;
 import model.Service.MemberService;
 import model.dto.MemberDto;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeUtility;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Properties;
 
 @WebServlet("/join")
 public class JoinController extends HttpServlet {
@@ -28,13 +33,57 @@ public class JoinController extends HttpServlet {
             String passwordVerify = request.getParameter("password-verify");
             String email = request.getParameter("email");
             String phone = request.getParameter("phone");
-//            if(!memberService.isExistId(id) && memberService.isPasswordVerified(password, passwordVerify)){
-//                if(memberService.isJoin(new MemberDto(name, id, password, email, phone))){
-//                    response.sendRedirect("login");
-//                    return;
-//                }
-//            }
-//        }
+
+            class MyAuthentication extends Authenticator{
+                PasswordAuthentication pa;
+                public MyAuthentication(){
+                    pa = new PasswordAuthentication("firstmind_@naver.com", "nsksdbwlsdl");
+                }
+
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return pa;
+                }
+            }
+
+            boolean isTranspot = false;
+            //SMTP 서버 주소
+            String smtpHost = "smtp.naver.com";
+
+            //받는 사람의 정보
+            String toName = name;
+            String toEmail = email;
+
+            //보내는 사람의 정보
+            String fromName = "박유진";
+            String fromEmail = "firstmind_@naver.com";
+
+            try{
+                Properties properties = new Properties();
+                properties.put("mail.smtp.host", smtpHost);
+                properties.put("mail.smtp.auth", "true");
+
+                //메일 인증
+                Authenticator myauth = new MyAuthentication();
+                Session session1 = Session.getInstance(properties, myauth);
+
+                InternetAddress address = new InternetAddress();
+                address.setPersonal(fromName, "UTF-8");
+                address.setAddress(fromEmail);
+
+                Message message = new MimeMessage(session1);
+                message.setFrom(address);
+                message.setSubject(MimeUtility.encodeText("WAP LIBRARY 회원가입 인증", "UTF-8", "B"));
+                message.setContent("왑 도서관 회원가입 인증 메일 테스트입니다.", "text/html;charset=utf-8");
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+
+                Transport.send(message);
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+            isTranspot = true;
+            request.setAttribute("isTransport", isTranspot);
+
             int check;
             if (memberService.isExistId(id)) {
                 check = 1;
@@ -45,8 +94,6 @@ public class JoinController extends HttpServlet {
                 memberService.isJoin(new MemberDto(name, id, password, email, phone));
             }
             request.setAttribute("check", check);
-            System.out.println(request.getCharacterEncoding());
-            System.out.println(response.getCharacterEncoding());
             RequestDispatcher requestDispatcher = request.getRequestDispatcher("JoinCase.jsp");
             requestDispatcher.forward(request, response);
         }
